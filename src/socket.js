155c2dc31928acc91277f26571085727c0306850
himanshu864 <himanshu864000@gmail.com>
2025-06-11 04:09:24 +0530

    minor url change

--- src/socket.js
+++ src/socket.js
@@ -1,6 +1,5 @@
 import { io } from "socket.io-client";
 
-// const URL = "http://localhost:3000";
-const URL = "http://192.168.151.52:3000";
+const URL = "http://localhost:3000";
 
 export const socket = io(URL);
