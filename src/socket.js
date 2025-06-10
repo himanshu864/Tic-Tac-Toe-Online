dad3a2c66cc41ab06b5f0abdeb0a99654f7ff46f
himanshu864 <himanshu864000@gmail.com>
2025-06-11 04:44:34 +0530

    major: implemented online gameplay multiplayer; serverless socket.io

--- src/socket.js
+++ src/socket.js
@@ -1,5 +1,8 @@
 import { io } from "socket.io-client";
 
-const URL = "http://localhost:3000";
+const SOCKET_URL = import.meta.env.VITE_SOCKET_URL || window.location.origin;
 
-export const socket = io(URL);
+export const socket = io(SOCKET_URL, {
+  path: "/api/socket",
+  transports: ["websocket"],
+});
