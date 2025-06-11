44160d6a7d7ef7201939afcb52750ce6a7a95c9c
himanshu864 <himanshu864000@gmail.com>
2025-06-11 06:18:35 +0530

    separate backend; added AWS URL

--- src/socket.js
+++ src/socket.js
@@ -1,8 +1,8 @@
 import { io } from "socket.io-client";
 
-const SOCKET_URL = import.meta.env.VITE_SOCKET_URL || window.location.origin;
+const SOCKET_URL = "http://3.110.20.10:3000";
 
 export const socket = io(SOCKET_URL, {
-  path: "/api/socket",
-  transports: ["websocket"],
+  path: "/socket.io",
+  transports: ["websocket", "polling"],
 });
