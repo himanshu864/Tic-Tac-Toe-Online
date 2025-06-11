8a8ec33d7daf746afaa20b19597fe5fe505686f8
himanshu864 <himanshu864000@gmail.com>
2025-06-11 13:20:14 +0530

    https chang

--- src/socket.js
+++ src/socket.js
@@ -1,6 +1,6 @@
 import { io } from "socket.io-client";
 
-const SOCKET_URL = "http://3.110.20.10:3000";
+const SOCKET_URL = "https://3.110.20.10:3000";
 
 export const socket = io(SOCKET_URL, {
   path: "/socket.io",
